b8277da1560e03736f67ec9ed390ecece112b1f5
Mahad Munir <mahadmunir6@gmail.com>
2025-06-18 13:09:38 +0500

    final update

--- backend/models/todo.js
+++ backend/models/todo.js
@@ -2,6 +2,11 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../db'); // import Sequelize instance
 
 const Todo = sequelize.define('Todo', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
   action: {
     type: DataTypes.STRING,
     allowNull: false,
